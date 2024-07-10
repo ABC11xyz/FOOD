b0ab09e173736f83c4b5147226d59280dee04d85
ABC@xyz <137751558+ABC11xyz@users.noreply.github.com>
2024-07-10 16:48:12 +0530

    Update App.jsx
    update

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -18,7 +18,7 @@ const App = () => {
       <div className="app-content">
         <Sidebar />
         <Routes>
-        <Route path="/" element={<Orders  />}  />
+        <Route path="/" element={<Orders url={url} />}  />
           <Route path="/add" element={<Add  url={url}/>}  />
           <Route path="/list" element={<List url={url}/>} />
           <Route path="/orders" element={<Orders url={url}/>} />
